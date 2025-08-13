504c32d973833d06f4ef48a9329b7f93abdf90f1
berkayy-atas <satis.destek@icredible.com>
2025-08-13 16:38:51 +0300

    Update configure_push.sh

--- scripts/restore/configure_push.sh
+++ scripts/restore/configure_push.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 cd repo-mirror
 
+sudo apt-get update && sudo apt-get install -y git-filter-repo
+
 if [ -z "$RESTORE_TOKEN" ]; then
   echo "::warning title=Information About The Scope Of Restoration::The repository will be restored without the ./.github/workflow directory. If you want to restore this directory, you can find the relevant steps at the following link: https://github.com/berkayy-atas/All-in-One-Repo-Repair-Kit?tab=readme-ov-file#step-1-create-a-personal-access-token"
-  sudo apt install git-filter-repo -y
   git filter-repo --force --path .github/workflows --invert-paths
   TOKEN="$GITHUB_TOKEN"
 else
+  git filter-repo --force --commit-callback 'pass'
   TOKEN="$RESTORE_TOKEN"
 fi
 
