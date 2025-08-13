268de3cb98943aeea0e95cd1c652f74a6074a268
berkayy-atas <satis.destek@icredible.com>
2025-08-13 17:03:31 +0300

    Update configure_push.sh

--- scripts/restore/configure_push.sh
+++ scripts/restore/configure_push.sh
@@ -4,18 +4,18 @@ cd repo-mirror
 sudo apt-get update && sudo apt-get install -y git-filter-repo
 
 if [ -z "$RESTORE_TOKEN" ]; then
-  echo "::warning title=Information About The Scope Of Restoration::The repository will be restored without the ./.github/workflow directory. If you want to restore this directory, you can find the relevant steps at the following link: https://github.com/berkayy-atas/All-in-One-Repo-Repair-Kit?tab=readme-ov-file#step-1-create-a-personal-access-token"
+  sudo apt-get install -y git-filter-repo
   git filter-repo --force --path .github/workflows --invert-paths
-  TOKEN="$GITHUB_TOKEN"
+  TOKEN_TO_USE="$GITHUB_TOKEN"
+  echo "::warning title=Information About The Scope Of Restoration::The repository will be restored without the ./.github/workflow directory. If you want to restore this directory, you can find the relevant steps at the following link: https://github.com/berkayy-atas/All-in-One-Repo-Repair-Kit?tab=readme-ov-file#step-1-create-a-personal-access-token"
 else
-  git filter-repo --force --commit-callback 'pass'
-  TOKEN="$RESTORE_TOKEN"
+  TOKEN_TO_USE="$RESTORE_TOKEN"
 fi
 
 echo $TOKEN
 
 git config user.name "myapp File Security"
 git config user.email "file-security@myapp.com"
-git push --mirror --force "https://x-access-token:$TOKEN@github.com/$GITHUB_REPOSITORY.git"
+git push --mirror --force "https://x-access-token:${TOKEN_TO_USE}@github.com/${{ github.repository }}.git"
 
 echo "::notice title=Success!::Repository restored successfully"
