38039fbbc4c7e68c0bf82e0457cf4b5776ce6673
berkayy-atas <satis.destek@icredible.com>
2025-08-20 18:42:09 +0300

    Update configure_push.sh

--- scripts/restore/configure_push.sh
+++ scripts/restore/configure_push.sh
@@ -3,7 +3,6 @@ set -e
 cd repo-mirror
 
 TOKEN_TO_USE=${REPOSITORY_RESTORATION_TOKEN:-$GITHUB_DEFAULT_TOKEN}
-[ -z "$REPOSITORY_RESTORATION_TOKEN" ]  && bash $ACTION_PATH/scripts/restore/filter_workflows.sh
 
 git config user.name "iCredible File Security"
 git config user.email "file-security@icredible.com"
