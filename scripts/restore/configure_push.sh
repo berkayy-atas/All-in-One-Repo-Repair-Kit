b61e079d38794fd035cd611e324ad675032f2b95
berkayy-atas <satis.destek@icredible.com>
2025-08-21 14:08:38 +0300

    Update configure_push.sh

--- scripts/restore/configure_push.sh
+++ scripts/restore/configure_push.sh
@@ -10,8 +10,7 @@ git config user.email "file-security@icredible.com"
 git remote set-url origin "https://$TOKEN_TO_USE@github.com/$GITHUB_REPOSITORY.git"
 
 git config --unset remote.origin.mirror
-git for-each-ref --format="%(refname)" --sort='authordate' | xargs git push origin
-git push --tags origin
+git push --mirror origin
 
 
 echo "::notice title=Success!::Repository restored successfully"
