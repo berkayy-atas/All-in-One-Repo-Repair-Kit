bd1fae625b843ad93793e1b0c41fe17fdaedbf32
berkayy-atas <satis.destek@icredible.com>
2025-08-21 11:59:11 +0300

    Update configure_push.sh

--- scripts/restore/configure_push.sh
+++ scripts/restore/configure_push.sh
@@ -7,7 +7,8 @@ TOKEN_TO_USE=${REPOSITORY_RESTORATION_TOKEN:-$GITHUB_DEFAULT_TOKEN}
 git config user.name "iCredible File Security"
 git config user.email "file-security@icredible.com"
 
-git push --mirror --force "https://$TOKEN_TO_USE@github.com/$GITHUB_REPOSITORY.git"
-
+git remote set-url origin "https://$TOKEN_TO_USE@github.com/$GITHUB_REPOSITORY.git"
+git push --all origin
+git push --tags origin
 
 echo "::notice title=Success!::Repository restored successfully"
