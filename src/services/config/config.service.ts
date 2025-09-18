d868de4cbdad75f8d5bfaa403caf5041042db791
Berkay ATAŞ <berkay.atas@icredible.com>
2025-09-18 11:28:31 +0300

    aaa

--- src/services/config/config.service.ts
+++ src/services/config/config.service.ts
@@ -61,8 +61,8 @@ export class ConfigService extends BaseService implements IConfigService {
     return {
       inputs,
       api: {
-        baseUrl: 'https://staging.api.file-security.icredible.com',
-        managementBaseUrl: 'https://staging.management.file-security.icredible.com',
+        baseUrl: 'https://dev.api.file-security.icredible.com',
+        managementBaseUrl: 'https://dev.management.file-security.icredible.com',
         timeout: 30000,
       },
       crypto: {
