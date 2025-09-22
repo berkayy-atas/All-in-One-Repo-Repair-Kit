52f2be2cfbfd561490d9317ce43ecd2334dedff7
Berkay ATAŞ <berkay.atas@icredible.com>
2025-09-22 13:40:21 +0300

    rrrr

--- src/services/workflow/restore-workflow.service.ts
+++ src/services/workflow/restore-workflow.service.ts
@@ -223,10 +223,11 @@ export class RestoreWorkflowService extends BaseService implements IRestoreWorkf
     // Push based on token type
     if (hasPatToken) {
       // PAT token can use mirror push
-      await this.gitService.pushMirror(repoPath, remoteUrl);
+      await this.gitService.pushAllBranches(repoPath);
+
     } else {
       // Default token push all branches individually
-      await this.gitService.pushAllBranches(repoPath);
+      await this.gitService.pushMirror(repoPath, remoteUrl);
     }
   }
 
